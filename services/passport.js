25fc8f66f37b4d74c9ad4857bc0fbcdccb48fe35
Eleanor Mark <eleanormark@gmail.com>
2017-08-26 20:22:02 -0700

    added cookie session

--- services/passport.js
+++ services/passport.js
@@ -1,18 +1,39 @@
 const passport = require('passport');
 const GoogleStategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((userid, done) => {
+  User.findById(userid).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
-    new GoogleStategy(
-      {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-      },
-      (accessToken, refreshToken, profile, done) => {
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile', profile);
-      }
-    )
-  );
+  new GoogleStategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //client profile id exist
+          done(null, existingUser);
+        } else {
+          //create new client record
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
