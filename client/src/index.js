f41064b775775366885d116ee85ed0f2c2dd5325
Eleanor Mark <eleanormark@gmail.com>
2017-09-01 19:29:05 -0700

    added and wired fetchUser action creator to App component

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,21 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+//Redux-Thunk allows us to write action creators witthout
+// having to immediately return an action.
+//Redux-Thunk givs us direct access to the dispatch function
+// this allows us to manually dispatch an action at anytime.
+import reduxThunk from 'redux-thunk';
+
+
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware());
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
