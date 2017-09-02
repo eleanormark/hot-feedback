685747deeee183a3f7fbeaf09f7c7e7c7f46aaf4
Eleanor Mark <eleanormark@gmail.com>
2017-09-02 15:59:52 -0700

    added payments component

---
+++ client/src/components/Payments.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+// import { connect } from 'react-redux';
+// import * as actions from '../actions';
+
+class Payments extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        name="Hot Feedback"
+        description="$5 for 5 email credits"
+        amount={500}
+        token={token => console.log("token",token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      />
+    );
+  }
+}
+
+export default Payments;
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-// keys.js
+// keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') {
     //in production - return prod set of keys
     module.exports = require('./prod');
