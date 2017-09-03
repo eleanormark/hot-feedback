e10641b3bad7e043888b2bbbd22fa9da5fd7c2e1
Eleanor Mark <eleanormark@gmail.com>
2017-09-03 01:35:01 -0700

    added heroku deployment snippets

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
 
@@ -25,6 +24,19 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  //Express will serve up production assets
+  //like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  //Express will serve up the index.html file
+  //if it does not recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
