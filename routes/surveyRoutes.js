f44be12b601b2205bc4fa2519c1fca090de0f9dd
Eleanor Mark <eleanormark@gmail.com>
2017-09-03 21:12:20 -0700

    fixed typos

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
-const Surveys = mongoose.model('surveys');
+const Survey = mongoose.model('surveys');
 
 module.exports = app => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
-    const survey = new Surveys({
+    const survey = new Survey({
       title,
       subject,
       body,
