24bf7d542a3941dd9e91be2582265130c3f6a50c
Eleanor Mark <eleanormark@gmail.com>
2017-09-05 15:19:52 -0700

    added Review Survey component

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,12 +1,30 @@
 // SurveyNew shows SurveyForm and SurveyForm Review
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+  // classic way of initializing component level state
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = { showFormReview: false };
+  // }
+
+  //Create React App has a babel plugin for initialiaing component level state
+  state = { showFormReview: false };
+
+  renderContent() {
+    if (this.state.showFormReview) {
+      return <SurveyFormReview />;
+    }
+    return <SurveyForm  onSurveySubmit={() => this.setState({showFormReview: false})}/>;
+  }
+
   render() {
     return (
       <div>
-        <SurveyForm />
+        {this.renderContent()}
       </div>
     );
   }
--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -1,4 +1,3 @@
-
 const re = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 export default emails => {
