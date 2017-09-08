bb8f6f1ee68ed95964045582b97df7b5783dcf28
Eleanor Mark <eleanormark@gmail.com>
2017-09-08 16:49:33 -0700

    render a list of surveys

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -12,7 +12,7 @@ import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
-  
+
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
